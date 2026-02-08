c8b3c49d7d6c2fb374874b5eb4d20397bbeef646
tikrack <tikrackcode@gmail.com>
2026-02-08 22:06:00 +0330

    fix

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/avestalang/core/lib"
+)
+
+func main() {
+	inputAddress := os.Args[1]
+
+	content, err := lib.Reader(inputAddress)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(content)
 }
