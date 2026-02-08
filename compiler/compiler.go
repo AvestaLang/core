ea5c8b8e29af4617780fca881edb4d8f0e40d2b4
tikrack <tikrackcode@gmail.com>
2026-02-08 22:31:51 +0330

    adding div

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"errors"
 	"strings"
 )
 
@@ -17,11 +18,46 @@ func Compile(input string) string {
 	for _, line := range lines {
 		trimmed := strings.TrimSpace(line)
 
+		if trimmed == "" {
+			continue
+		}
+
 		if strings.HasSuffix(trimmed, ":") {
-			println("Tag Finded")
+			translated, err := translate(strings.Trim(trimmed, ":"))
+
+			if translated == "" {
+				continue
+			}
+
+			if err != nil {
+				panic(err)
+			}
+
+			result.WriteString("<" + translated + ">")
+		}
+
+		if strings.Contains(trimmed, "=") && strings.Contains(trimmed, "«") && strings.Contains(trimmed, "»") {
+			println("Property Finded")
+		}
+
+		if trimmed == "پایان" {
+			println("End Finded")
 		}
 	}
 
 	result.WriteString("</body></html>")
 	return result.String()
+}
+
+func translate(tag string) (string, error) {
+	tags := map[string]string{
+		"دکمه": "button",
+		"جعبه": "div",
+	}
+
+	if hTag, exists := tags[tag]; exists {
+		return hTag, nil
+	}
+
+	return "", errors.New("Cant find translate of => " + tag)
 }
